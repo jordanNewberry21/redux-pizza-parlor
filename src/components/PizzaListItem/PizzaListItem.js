d25404808445ae559e790eb3d756e02cc09d08de
Josie Fredericksen <jfredericksen12@gmail.com>
2020-12-10 11:27:36 -0600

    addpizza to cart function

--- src/components/PizzaListItem/PizzaListItem.js
+++ src/components/PizzaListItem/PizzaListItem.js
@@ -4,13 +4,39 @@ import {connect} from 'react-redux';
 
 //class
 class PizzaListItem extends Component{
+
+    state = {
+        buttonClick: true
+    }
+
+    addPizza = () => {
+        console.log('in addPizza function cause i clicked the button.....')
+        this.setState({
+            buttonClick: !this.state.buttonClick
+        })
+        this.props.dispatch( { type: 'CHECKOUT_PIZZA', payload: this.props.pizza})
+        console.log('Added pizza to cart', this.props.pizza);
+        
+    }
+
+    removePizza = () => {
+        console.log('hello from remove pizza')
+        this.setState({
+            buttonClick: !this.state.buttonClick
+        })
+    }
+
     render(){
         return(
             <div className="pizza">
-                <img src={this.props.pizza.image_path} alt="pizza pictures"></img>
+                {/* <img src={this.props.pizza.image_path} alt="pizza pictures"></img> */}
                 <h2>{this.props.pizza.name}</h2>
                 <h4>{this.props.pizza.description}</h4>
                 <p>{this.props.pizza.price}</p>
+                {this.state.buttonClick ?
+                    <button onClick={this.addPizza}>Add Pizza</button>  : 
+                    <button onClick={this.removePizza}>Remove Pizza</button>}
+                
             </div>
         ) //end return 
     } //end render
