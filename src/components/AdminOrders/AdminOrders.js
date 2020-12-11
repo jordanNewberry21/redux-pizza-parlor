1cd8acc11a8f10f1bceb529428a345ea88017c91
John Micko <jrmicko5012@gmail.com>
2020-12-11 10:04:25 -0600

    get order history from server and send to admin

--- src/components/AdminOrders/AdminOrders.js
+++ src/components/AdminOrders/AdminOrders.js
@@ -3,10 +3,19 @@ import React, { Component } from 'react';
 
 //class
 class AdminOrders extends Component{
+
+    componentDidMount(){
+        console.log('admin order mounted');
+        this.props.getOrders()
+    }
+
     render(){
         return(
             <div>
                 <h2>Hello from AdminOrders component </h2>
+                {this.props.orderHistory.map( order => 
+                    <p key={order.id} >hey there</p>
+                    )}
             </div>
         ) //end return 
     } //end render
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,19 +9,41 @@ import { connect } from 'react-redux';
 import Header from '../Header/Header';
 import Checkout from '../Checkout/Checkout';
 
+
 class App extends Component {
+
+  state = {
+    orderHistory: []
+  }
+
+  getOrders = () => {
+    axios.get('/api/order')
+      .then((response) => {
+        this.setState({
+          orderHistory: response.data
+        })
+        console.log('back from server', this.state.orderHistory);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert('problem with GET');
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <Header/>
-        <br/>
-        <img src="images/pizza_photo.png"/>
-        <p>Pizza is great.</p> 
-        <Router> 
-          <Route exact path="/" component={Menu}/>
-          <Route exact path="/customer-form" component={CustomerForm}/>
-          <Route path="/admin" component={AdminOrders}/>
-          <Route path="/checkout" component={Checkout}/>
+        <Header />
+        <br />
+        <img src="images/pizza_photo.png" />
+        <p>Pizza is great.</p>
+        <Router>
+          <Route exact path="/" component={Menu} />
+          <Route exact path="/customer-form" component={CustomerForm} />
+          <Route path="/admin">
+            <AdminOrders getOrders={this.getOrders} orderHistory={this.state.orderHistory} />
+          </Route>
+          <Route path="/checkout" component={Checkout} />
         </Router>
       </div>
     );
